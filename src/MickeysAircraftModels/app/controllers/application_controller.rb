de661f891e6650ee69732189c51030dc50e74143
Michael <mdart86@hotmail.com>
2021-03-18 23:11:26 +1000

    additions of supporting documentation

--- src/MickeysAircraftModels/app/controllers/application_controller.rb
+++ src/MickeysAircraftModels/app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+
+    # def after_sign_up_path_for(resource)
+    #     redirect_to new_product_path
+    # end
 end
